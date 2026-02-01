5052462fa8d210bc2a5cf44f515828ec54f589bc
joyfive <joy_five@kakao.com>
2026-02-01 16:45:32 +0900

    style: 마이너한 스타일링, 메타태그 수정

--- src/app/blog/[category]/[path]/page.tsx
+++ src/app/blog/[category]/[path]/page.tsx
@@ -1,6 +1,6 @@
 // src/app/blog/[category]/[path]/page.tsx
 import { fetchPostByPath } from "@/lib/notion/fetchPostByPath";
-import { getTitle } from "@/lib/utils/post";
+import { getTitle, getMultiSelect, getDate } from "@/lib/utils/post";
 import { notFound } from "next/navigation";
 import { NotionDetailRenderer } from "@/components/notion/NotionDetailRenderer"; // 방금 만든 것
 import Link from "next/link";
@@ -16,14 +16,25 @@ export default async function PostDetailPage({ params }: Props) {
   if (!postData) return notFound();
 
   const title = getTitle(postData.properties.title);
-
+  const tags = getMultiSelect(postData.properties.tags);
+  const updated_at = getDate(postData.properties.updated_at);
   return (
     <main className="max-w-4xl mx-auto py-12 px-4">
-      <header className="mb-10">
+      <header className="mb-10 flex flex-col gap-2">
         <div className="text-sm text-gray-400 mb-2">
           <Link href={`/blog/${category}`}>  {category} </Link>
+
         </div>
-        <h1 className="text-4xl font-bold">{title}</h1>
+        <div className="flex items-center justify-between"><h1>{title}</h1>
+          <div className="text-sm text-gray-400 mb-2">
+            게시일: {new Date(updated_at).toLocaleDateString("ko-KR")}
+          </div></div>
+        <div className="flex items-center gap-2">
+          {tags.map((tag) => (
+            <span key={tag} className="text-xs text-gray-500 bg-gray-100 rounded-sm px-2 py-1 border border-gray-200">
+              # {tag}
+            </span>
+          ))}</div>
       </header>
 
       <article className="notion-content">
